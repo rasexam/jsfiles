989be2fdb0b86a81effc4d8bb2fb9c58a595d2dd
rasexam <raspre2013@gmail.com>
2013-12-21 19:16:53 +0530

    Create rasjs11.js

---
+++ rasjs11.js
@@ -0,0 +1,17 @@
+
+     /* No = 11 */
+                var disqus_shortname = &#39;rasmainexam&#39;;
+                var disqus_blogger_current_url = &quot;<data:blog.canonicalUrl/>&quot;;
+
+                if (!disqus_blogger_current_url.length) {
+                    disqus_blogger_current_url = &quot;<data:blog.url/>&quot;;
+                }
+
+                var disqus_blogger_homepage_url = &quot;<data:blog.homepageUrl/>&quot;;
+                var disqus_blogger_canonical_homepage_url = &quot;<data:blog.canonicalHomepageUrl/>&quot;;
+            </script>
+
+            <b:if cond='data:blog.pageType == &quot;item&quot;'>
+                <style type='text/css'>
+                    #comments {display:none;}
+              
