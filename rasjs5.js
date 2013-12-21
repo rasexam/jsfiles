506f078dc0085f96998d1a42c591cb12fe937327
rasexam <raspre2013@gmail.com>
2013-12-21 19:13:54 +0530

    Create rasjs5.js

---
+++ rasjs5.js
@@ -0,0 +1,9 @@
+
+ /* No = 5 */
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = &quot;//connect.facebook.net/en_US/all.js#xfbml=1&quot;;
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, &#39;script&#39;, &#39;facebook-jssdk&#39;));
