b7ba4678f2ef346bf8cbd8d7ea6cfaa3f6cf00c5
rasexam <raspre2013@gmail.com>
2013-12-21 19:19:09 +0530

    Create rasjs14.js

---
+++ rasjs14.js
@@ -0,0 +1,11 @@
+     
+         /* No = 14 */
+                (function() {
+                    var bloggerjs = document.createElement(&#39;script&#39;);
+                    bloggerjs.type = &#39;text/javascript&#39;;
+                    bloggerjs.async = true;
+                    bloggerjs.src = &#39;http://&#39;+disqus_shortname+&#39;.disqus.com/blogger_index.js&#39;;
+                    (document.getElementsByTagName(&#39;head&#39;)[0] || document.getElementsByTagName(&#39;body&#39;)[0]).appendChild(bloggerjs);
+                })();
+
+      
